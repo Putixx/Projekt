665ecfb3292af58089a42df42aa131d11f9766d3
Putixx <80105950+Putixx@users.noreply.github.com>
2023-02-08 17:55:03 +0100

    Dodaj pliki projektów.

---
+++ Czworacze/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace Czworacze
+{
+    class Program
+    {
+        public static bool[] Przesiej(long n)
+        {
+            bool[] tab = new bool[n];
+
+            for (int i = 2; i * i < n; i++)
+            {                           
+                if (!tab[i])
+                {
+                    for (int j = i * i; j < n; j += i)
+                    {
+                        tab[j] = true;
+                    }
+                }      
+            }
+
+            return tab;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Podaj zakres górny: ");
+
+            bool[] result = Przesiej(Convert.ToInt64(Console.ReadLine()));
+            StringBuilder output = new StringBuilder();
+            output.AppendLine("Liczby czworacze:");
+
+            for (int i = 2; i < result.Length - 8; i++)
+            {
+                if (!result[i] && !result[i + 2] && !result[i + 6] && !result[i + 8])
+                {
+                    output.AppendLine(i + ", " + (i + 2) + ", " + (i + 6) + ", " + (i + 8));
+                }
+            }
+
+            Console.WriteLine("\n" + output);
+        }
+
+    }
+}
